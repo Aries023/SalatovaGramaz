19a47b378b8427fac3f3b51a49d44e14343b4a98
Aries023 <pet.potocka@gmail.com>
2022-08-08 05:37:18 +0200

    renamed button, created another one and gived them functions + created Text field for take values (also Idea wannted me update version of maven before commit)

--- src/main/java/com/example/salatovagramaz/HelloController.java
+++ src/main/java/com/example/salatovagramaz/HelloController.java
@@ -2,13 +2,24 @@ package com.example.salatovagramaz;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+    @FXML
+    private TextField numbOfSack;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onVelkySal() {
+        int piece = Integer.parseInt(numbOfSack.getText()) * 570 / 100; // kolko salatov spravim
+        int ostatok = Integer.parseInt(numbOfSack.getText()) * 570 % 100; // kolko zostane gramaze
+        welcomeText.setText("z "+ numbOfSack.getText() +" sackov bude "+piece + "salatov a ostane "+ostatok+" gramaze");
+    }
+    @FXML
+    protected void onMalySal() {
+        int piece = Integer.parseInt(numbOfSack.getText()) * 570 / 60; // kolko salatov spravim
+        int ostatok = Integer.parseInt(numbOfSack.getText()) * 570 % 60;// kolko zostane gramaze
+        welcomeText.setText("z "+ numbOfSack.getText() +" sackov bude "+piece + "salatov a ostane "+ostatok+" gramaze");
     }
 }
