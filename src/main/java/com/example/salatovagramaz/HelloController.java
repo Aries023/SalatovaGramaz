45077a9157ab4ea9317eb8da35f9c5fc3b943aa0
Aries023 <pet.potocka@gmail.com>
2022-08-11 13:16:17 +0200

    zabranil som uzivatelovy aby nezadal ni alebo nieco ine ako cislicu vdaka matches

--- src/main/java/com/example/salatovagramaz/HelloController.java
+++ src/main/java/com/example/salatovagramaz/HelloController.java
@@ -12,14 +12,24 @@ public class HelloController {
 
     @FXML
     protected void onVelkySal() {
-        int piece = Integer.parseInt(numbOfSack.getText()) * 570 / 100; // kolko salatov spravim
-        int ostatok = Integer.parseInt(numbOfSack.getText()) * 570 % 100; // kolko zostane gramaze
-        welcomeText.setText("z "+ numbOfSack.getText() +" sackov bude "+piece + "salatov a ostane "+ostatok+" gramaze");
+        if (numbOfSack.getText().matches("[0-9]+")){
+            int piece = Integer.parseInt(numbOfSack.getText()) * 570 / 100; // kolko salatov spravim
+            int ostatok = Integer.parseInt(numbOfSack.getText()) * 570 % 100; // kolko zostane gramaze
+            welcomeText.setText("z "+ numbOfSack.getText() +" sackov bude "+piece + "salatov a ostane "+ostatok+" gramaze");
+        }else {
+            welcomeText.setText("pravdepodobne si nezadal čislo alebo si zadal nejaky text.");
+        }
+
     }
     @FXML
     protected void onMalySal() {
-        int piece = Integer.parseInt(numbOfSack.getText()) * 570 / 60; // kolko salatov spravim
-        int ostatok = Integer.parseInt(numbOfSack.getText()) * 570 % 60;// kolko zostane gramaze
-        welcomeText.setText("z "+ numbOfSack.getText() +" sackov bude "+piece + "salatov a ostane "+ostatok+" gramaze");
+        if (numbOfSack.getText().matches("[0-9]+")){
+            int piece = Integer.parseInt(numbOfSack.getText()) * 570 / 60; // kolko salatov spravim
+            int ostatok = Integer.parseInt(numbOfSack.getText()) * 570 % 60;// kolko zostane gramaze
+            welcomeText.setText("z "+ numbOfSack.getText() +" sackov bude "+piece + "salatov a ostane "+ostatok+" gramaze");
+        }else {
+            welcomeText.setText("pravdepodobne si nezadal čislo alebo si zadal nejaky text.");
+        }
+
     }
 }
